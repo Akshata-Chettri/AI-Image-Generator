e5fcd2e522ed5a46393f6f65f9dd0d78376ae8ed
Akshata Chettri <akshatachettri10@gmail.com>
2023-02-09 19:37:54 +0530

    Updated Code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,11 +50,12 @@ const Home = () => {
     }, []);
 
     const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout);
         setSearchText(e.target.value);
 
         setSearchTimeout(
             setTimeout(() => {
-                const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase.includes(searchText.toLowerCase()));
+                const searchResults = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
 
                 setSearchedResults(searchResults)
             }, 500)

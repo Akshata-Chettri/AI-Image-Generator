4849124c8cdd05c73b5468348113dd976a5aeb3e
Akshata Chettri <akshatachettri10@gmail.com>
2024-03-04 20:38:14 +0530

    removed model and added log for api call

--- server/routes/dallERoutes.js
+++ server/routes/dallERoutes.js
@@ -21,13 +21,12 @@ router.route('/').post(async (req, res) => {
         const { prompt } = req.body;
 
         const aiResponse = await openai.createImage({
-            model: 'dall-e-3',
             prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
         });
-
+        console.log("API call success: ", aiResponse.data);
         const image = aiResponse.data.data[0].b64_json;
 
         res.status(200).json({ photo: image });
